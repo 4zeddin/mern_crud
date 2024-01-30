a70897e8a9e35e1bf6856d8412d3a34a37d0f369
4zeddin <azeddinaitwaalla@gmail.com>
2024-01-30 12:44:38 +0100

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,13 +14,13 @@ const App = () => {
         console.log(err.message);
       }
     };
-    
+
     fetchApi();
   }, []);
-  const addTodo = async(e) => {
+  const addTodo = async (e) => {
     e.preventDefault();
-    if( input.length === 0 ) return null;
-    await axios.post("/todos",[
+    if (input.length === 0) return null;
+    const res = await apiService.post("/todos", [
       {
         ...todos,
         text: input,
@@ -29,18 +29,41 @@ const App = () => {
     ]);
     setInput("");
     fetchApi();
-  }
+    return res.data;
+  };
+  const handleDelete = async (id) => {
+    const res = await apiService.delete(`todos/${id}`, { id });
+    fetchData();
+    return res.data;
+  };
+  const handleUpdate = async (id) => {
+    const res = await apiService.put(`todos/${id}`, { id });
+  };
   return (
-    <main>
+    <main className="bg-slate-950 min-h-screen">
+      <input
+        type="text"
+        className="ms-6 my-6 w-5/12 py-2 px-4 rounded-l-md border border-gray-300 focus:outline-none focus:border-blue-500"
+        value=""
+        placeholder="Add your Todo"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        className="py-2 px-4 bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none"
+        onClick={(e) => addTodo(e)}
+      >
+        Add
+      </button>
       <div>
-        <input
-          type="text"
-          value=""
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={(e)=>addTodo(e)}>Add</button>
+        {todos.map((t) => {
+          <div key={t._id}>
+            <p className="cursor-pointer" onClick={handleUpdate(t._id)}>
+              {t.text}
+            </p>
+            <button onClick={handleDelete(t._id)}>X</button>
+          </div>;
+        })}
       </div>
-      <div>{/* {todo.map()} */}</div>
     </main>
   );
 };
